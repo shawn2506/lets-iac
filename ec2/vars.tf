0ed464a9050b5d6989e5ff95f555c48d7cfe9728
shawn2506 <dsouzashawn62@yahoo.in>
2019-11-09 15:04:27 +0530

    Setup a basic EC2 instance with TF-0.12

---
+++ ec2/vars.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "ami" {
+  type = map(string)
+  default = {
+    us-east-1  = "ami-04b9e92b5572fa0d1"
+    ap-south-1 = "ami-0123b531fc646552f "
+  }
+}
